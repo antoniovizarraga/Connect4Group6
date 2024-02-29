8f3a5bd6458b247615f37427a53f876c328efb3c
rgarcia <rgarcia@106-06.info2>
2024-02-29 13:44:03 +0100

    impresión del tablero todavía sin comentar

--- src/connect4game/Game.java
+++ src/connect4game/Game.java
@@ -2,4 +2,39 @@ package connect4game;
 
 public class Game {
 	// Esta clase es donde va el juego
+	/**
+	 * Function for painting the board of the game
+	 * @param board The board to paint
+	 */
+	public static void display(char[][] board){
+		/*
+		 * Loop where the number at the top of the board is printed
+		 */
+		for(int col = 0; col < board.length; col++) {
+			/*
+			 * Print the number and if its zero add an extra white space
+			 */
+			if(col == 0) {
+				System.out.print("   " + col + " ");
+			}else {
+				System.out.print(col + " ");
+			}
+		}
+		// Line break to start printing the rest of the board
+		System.out.println();
+		
+		/*
+		 * Loop where the board is printed
+		 */
+		for (int row = 0; row < board.length; row++) {
+			System.out.print(row + " ");
+			System.out.print("|");
+			for (int col = 0; col < board[0].length; col++) {
+				System.out.print(board[row][col]);
+				System.out.print("|");
+			}
+			System.out.println();
+		}
+		System.out.println();
+	}
 }
--- src/connect4game/Main.java
+++ src/connect4game/Main.java
@@ -5,6 +5,20 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		/*
+		 * Array where the board size is saved and it couldn't be mora than ten
+		 */
+		char[][] board = new char[10][10];
+
+		// initialize array
+		for (int row = 0; row < board.length; row++) {
+			for (int col = 0; col < board[0].length; col++) {
+				board[row][col] = ' ';
+			}
+		}
+		
+		Game.display(board);
+
 	}
 
 }
