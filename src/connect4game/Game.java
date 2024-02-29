1b5083a0463b2604efa0911bc0a721f1447ae210
Sgonzalez <Sgonzalez@106-07.info2>
2024-02-29 13:38:32 +0100

    Función validate completa Vers. Final

--- src/connect4game/Game.java
+++ src/connect4game/Game.java
@@ -3,19 +3,20 @@ package connect4game;
 public class Game {
 	// Esta función comprueba si el valor introducido está dentro del tablero.
 	public static boolean validate(int column, char[][] board){
+		//creamos la variable condition inicializada a true para guardar lo que devolvemos.
+		boolean condition=true;
 		
-		
-		//Si el valor de la columna es menor que 0 o mayor que el tablero devolverá falso.
+		//Si el valor de la columna es menor que 0 o mayor que el tablero condition pasará a ser false.
 		if (column < 0 || column > board[0].length){
-			return false;
+			condition= false;
 		}
 		
-		//Si el hueco no está vacío devolverá falso.
+		//Si el hueco no está vacío condition pasará a ser false.
 		if (board[0][column] != ' '){
-			return false;
+			condition= false;
 		}
-		//En caso contrario devolverá verdadero
-		return true;
+		//Devuelve la variable condition
+		return condition;
 	}
 	
 }
