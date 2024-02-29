ba2ebe34c165b402bf1262add108b272a4b46c59
dvi26 <david.caro@iesnervion.es>
2024-02-29 13:00:43 +0100

    c1

--- src/connect4game/Game.java
+++ src/connect4game/Game.java
@@ -2,4 +2,12 @@ package connect4game;
 
 public class Game {
 	// Esta clase es donde va el juego
+	
+	//method david
+	public static boolean isWinner(char player, char[][] grid){
+		boolean condition;
+		return condition=false;
+	}
 }
+
+
