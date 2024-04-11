9a4898378f85241d8ddff6cf6eca6af6cac8d069
dvi26 <david.caro@iesnervion.es>
2024-04-11 12:40:15 +0200

    CF

--- src/connect4game/Game.java
+++ src/connect4game/Game.java
@@ -52,29 +52,30 @@ public class Game {
 	// method that will check if there's a winner, input parameters are the player
 	// (if there's a checker) and the board itself.
 	public static boolean isWinner(char player, char[][] board) {
-		// Initialize the variable condition as false (there's no winner).
-		boolean condition = false;
 		// For loop that will check if there're 4 checkers one after another in a row
+		boolean condition = false;
 		for (int row = 0; row < board.length; row++) {
 			for (int col = 0; col < board[0].length - 3; col++) {
 				// player represents the checker
 				if (board[row][col] == player && board[row][col + 1] == player && board[row][col + 2] == player
 						&& board[row][col + 3] == player) {
-					// if the condition is met it changes it to true
+					// if the condition is met it returns true
 					condition = true;
 				}
 			}
 		}
+
 		// For loop that will check if there're 4 checkers in a column
-		for (int row = 0; row < board.length; row++) {
-			for (int col = 0; col < board[0].length - 3; col++) {
-				if (board[row][col] == player && board[row][col + 1] == player && board[row][col + 2] == player
-						&& board[row][col + 3] == player) {
+		for (int row = 0; row < board.length - 3; row++) {
+			for (int col = 0; col < board[0].length; col++) {
+				if (board[row][col] == player && board[row + 1][col] == player && board[row + 2][col] == player
+						&& board[row + 3][col] == player) {
 					condition = true;
 				}
 			}
 		}
-		// For loop that will check if there're 4 checkers in a upward diagonally
+
+		// For loop that will check if there're 4 checkers in an upward diagonal
 		for (int row = 3; row < board.length; row++) {
 			for (int col = 0; col < board[0].length - 3; col++) {
 				if (board[row][col] == player && board[row - 1][col + 1] == player && board[row - 2][col + 2] == player
@@ -83,10 +84,10 @@ public class Game {
 				}
 			}
 		}
-		// check downward diagonally
+
+		// Check downward diagonally
 		for (int row = 0; row < board.length - 3; row++) {
 			for (int col = 0; col < board[0].length - 3; col++) {
-
 				if (board[row][col] == player && board[row + 1][col + 1] == player && board[row + 2][col + 2] == player
 						&& board[row + 3][col + 3] == player) {
 					condition = true;
@@ -94,7 +95,6 @@ public class Game {
 			}
 		}
 		return condition;
-
 	}
 
 	// This function checks if the entered value is correct, returning true if
